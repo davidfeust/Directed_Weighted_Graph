2be77014e043c71ca039a1848e3c96c2375454eb
davidfeust <73194236+davidfeust@users.noreply.github.com>
2020-11-30 00:01:50 +0200

    trys

--- src/gameClient/Ex2.java
+++ src/gameClient/Ex2.java
@@ -2,7 +2,7 @@ package gameClient;
 
 
 import trys.GUI_102;
-
+import api.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
@@ -11,21 +11,30 @@ import java.awt.event.WindowEvent;
 public class Ex2{
 
     public static void main(String[] args) {
-        JFrame jf = new JFrame();
-
-        jf.setSize(400,400);
-        JButton b = new JButton();
-        jf.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.out.println("done");
-                System.exit(0);
-            }
-        });
-        b.setSize(10,20);
-        Container contentPane = jf.getContentPane();
-        contentPane.add(b);
-        jf.show();
-
+//        JFrame jf = new JFrame("Pockemons Game");
+//        jf.setSize(400,400);
+//        jf.addWindowListener(new WindowAdapter() {
+//            public void windowClosing(WindowEvent e) {
+//                System.out.println("done");
+//                System.exit(0);
+//            }
+//        });
+//        ImageIcon img = new ImageIcon("src/ex2/img/pokeball.png");
+//        jf.setIconImage(img.getImage());
+////        jf.addWindowListe;
+//        Container contentPane = jf.getContentPane();
+//        jf.show();
+        GameGUI gui = new GameGUI(4);
+        Arena ar = new Arena();
+        directed_weighted_graph g = new WDGraph_DS();
+        g.addNode(new NodeData(1));
+        g.addNode(new NodeData(2));
+        g.addNode(new NodeData(3));
+        g.addNode(new NodeData(4));
+        ar.setGraph(g);
+        gui.set_ar(ar);
+//        gui.paint();
+        gui.show();
     }
 
 }
--- src/gameClient/GameGUI.java
+++ src/gameClient/GameGUI.java
@@ -1,17 +1,70 @@
 package gameClient;
 
+import api.*;
+import gameClient.util.Range;
+import gameClient.util.Range2D;
+import gameClient.util.Range2Range;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GameGUI extends JFrame {
 
     private Arena _ar;
+    private int _scenario_num;
+    private gameClient.util.Range2Range _w2f;
 
-    public GameGUI(String name) {
-        super(name);
+    public GameGUI(int scenario_num) {
+        super("Pockemons Game" + scenario_num);
+        _scenario_num = scenario_num;
+        setSize(800,400);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.out.println("done");
+                System.exit(0);
+            }
+        });
+    }
+
+    private void updateFrame() {
+        Range rx = new Range(20,this.getWidth()-20);
+        Range ry = new Range(this.getHeight()-10,150);
+        Range2D frame = new Range2D(rx,ry);
+        directed_weighted_graph g = _ar.getGraph();
+        _w2f = Arena.w2f(g,frame);
+    }
+
+    public void paint(Graphics g) {
+        updateFrame();
+        int w = this.getWidth();
+        int h = this.getHeight();
+        g.clearRect(0, 0, w, h);
+
+        //	updateFrame();
+//        drawPokemons(g);
+        drawGraph(g);
+//        drawAgants(g);
+//        drawInfo(g);
+    }
+
+    private void drawGraph(Graphics g) {
+        directed_weighted_graph graph = _ar.getGraph();
+        for (node_data i : graph.getV()) {
+            drawNode(i, 0, g);
+        }
     }
 
     public void set_ar(Arena _ar) {
         this._ar = _ar;
+    }
+
+    private void drawNode(node_data n, int r, Graphics g) {
+        geo_location pos = n.getLocation();
+        geo_location fp = this._w2f.world2frame(pos);
+        g.fillOval((int)fp.x()-r, (int)fp.y()-r, 2*r, 2*r);
+        g.drawString(""+n.getKey(), (int)fp.x(), (int)fp.y()-4*r);
     }
 
 
