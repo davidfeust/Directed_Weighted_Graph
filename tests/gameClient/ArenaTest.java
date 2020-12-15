804cc53de3b03ebaa2aaa1a11d3e791165bc4116
davidfeust <73194236+davidfeust@users.noreply.github.com>
2020-12-15 03:06:23 +0200

    add test

---
+++ tests/gameClient/ArenaTest.java
@@ -0,0 +1,60 @@
+package gameClient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArenaTest {
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void updateGraph() {
+    }
+
+    @Test
+    void updatePokemons() {
+    }
+
+    @Test
+    void updateAgents() {
+    }
+
+    @Test
+    void w2f() {
+    }
+
+    @Test
+    void getAgents() {
+    }
+
+    @Test
+    void getPokemons() {
+    }
+
+    @Test
+    void get_pokemonsWithOwner() {
+    }
+
+    @Test
+    void get_graph() {
+    }
+
+    @Test
+    void get_timeStart() {
+    }
+
+    @Test
+    void set_timeStart() {
+    }
+
+    @Test
+    void getTime() {
+    }
+
+    @Test
+    void getGrade() {
+    }
+}
